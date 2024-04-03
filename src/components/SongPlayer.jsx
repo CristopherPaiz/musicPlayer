8627f4e1a4bdc90306274b2dfd6c55660ed61d8f
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-04-03 10:08:15 -0600

    test

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -5,7 +5,7 @@ const SongPlayer = () => {
   const [artist] = useState("El Trono de Mexico");
   const [nameSong] = useState("La Ciudad Del Olvido");
   const [urlSong, setUrlSong] = useState(
-    `https://rayluo.github.io/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/1.mp3`
+    `https://github.com/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/1.mp3`
   );
   //   const [urlSong, setUrlSong] = useState(
   //     `https://github.com/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/1.mp3`
@@ -14,7 +14,7 @@ const SongPlayer = () => {
 
   //https://raw.githack.com/brython-dev/brython/master/www/tests/index.html
   const playNextFragment = () => {
-    const newUrlSong = `https://rayluo.github.io/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/${fragment}.mp3`;
+    const newUrlSong = `https://github.com/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/${fragment}.mp3`;
     // const newUrlSong = `https://github.com/CristopherPaiz/musicPlayer/raw/main/src/Music/${artist}/${nameSong}/${fragment}.mp3`;
     const fragment = Math.floor(Math.random() * totalFragments) + 1;
     setUrlSong(encodeURI(newUrlSong));
