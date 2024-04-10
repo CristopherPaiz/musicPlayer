14e3bae15a4ce749ce03267c4874d19a8414b4b7
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-04-10 15:45:11 -0600

    Refactor Lyrics component to display multiple lines

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const Lyrics = ({ lyrics, timeElapsed }) => {
-  const [currentLine, setCurrentLine] = useState("");
-  const [previousLine, setPreviousLine] = useState("");
-  const [nextLine, setNextLine] = useState("");
+  const [currentLines, setCurrentLines] = useState(Array(11).fill(""));
 
   useEffect(() => {
     const lines = lyrics.split("\n");
@@ -15,9 +13,13 @@ const Lyrics = ({ lyrics, timeElapsed }) => {
       const seconds = parseFloat(time.split(":")[1]);
       const totalTimeInSeconds = minutes * 60 + seconds;
       if (totalTimeInSeconds > timeElapsedInSeconds) {
-        setCurrentLine(i > 0 ? lines[i - 1].substring(10) : "");
-        setPreviousLine(i > 1 ? lines[i - 2].substring(10) : "");
-        setNextLine(i < lines.length ? lines[i].substring(10) : "");
+        let newLines = Array(11).fill("");
+        for (let j = -5; j <= 5; j++) {
+          if (i + j >= 0 && i + j < lines.length) {
+            newLines[j + 6] = lines[i + j].substring(10);
+          }
+        }
+        setCurrentLines(newLines);
         break;
       }
     }
@@ -27,25 +29,31 @@ const Lyrics = ({ lyrics, timeElapsed }) => {
     <div
       style={{
         width: "400px",
-        height: "400px",
-        padding: "10px",
+        height: "300px",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         gap: "10px",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        overflow: "hidden",
+        marginBottom: "100px",
+        padding: "20px 10px 10px 10px",
+        textWrap: "wrap",
       }}
     >
-      <p>{previousLine}</p>
-      <p style={{ color: "red" }}>{currentLine}</p>
-      <p>{nextLine}</p>
+      {currentLines.map((line, index) => (
+        <p key={index} style={{ color: index === 5 ? "red" : "white", padding: "0", lineHeigh: "1", margin: "5px" }}>
+          {line}
+        </p>
+      ))}
     </div>
   );
 };
 
 Lyrics.propTypes = {
   lyrics: PropTypes.string.isRequired,
-  timeElapsed: PropTypes.string.isRequired,
+  timeElapsed: PropTypes.number.isRequired,
 };
 
 export default Lyrics;
